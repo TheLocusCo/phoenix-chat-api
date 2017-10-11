72f3639bf92623849bf0cbb95439eed01fc95e4f
Louis Alridge <loualrid@gmail.com>
2017-10-11 09:23:41 -0700

    Added message and user model tests

--- test/models/message_test.exs
+++ test/models/message_test.exs
@@ -15,4 +15,22 @@ defmodule PhoenixChat.MessageTest do
     changeset = Message.changeset(%Message{}, @invalid_attrs)
     refute changeset.valid?
   end
+
+  test "query that returns latest messages of a given room" do
+    first  = Ecto.DateTime.from_erl({{2016, 5, 23}, {12, 30, 12}})
+    second = Ecto.DateTime.from_erl({{2016, 5, 24}, {12, 30, 12}})
+    third  = Ecto.DateTime.from_erl({{2016, 5, 25}, {12, 30, 12}})
+    Repo.insert!(%Message{room: "1", body: "test", timestamp: first})
+    Repo.insert!(%Message{room: "1", body: "test", timestamp: second})
+    Repo.insert!(%Message{room: "1", body: "test", timestamp: third})
+    Repo.insert!(%Message{room: "2", body: "test", timestamp: Ecto.DateTime.utc()})
+
+    messages = Message.latest_room_messages("1", 2) |> Repo.all
+    assert length(messages) == 2
+
+    [msg1 | [msg2]] = messages
+    assert Ecto.DateTime.compare(msg2.timestamp, second) == :eq
+    assert Ecto.DateTime.compare(msg1.timestamp, msg2.timestamp) == :gt
+    assert {10, :integer} in Message.latest_room_messages("1").limit.params
+  end
 end
--- test/models/user_test.exs
+++ test/models/user_test.exs
@@ -15,4 +15,53 @@ defmodule PhoenixChat.UserTest do
     changeset = User.changeset(%User{}, @invalid_attrs)
     refute changeset.valid?
   end
+
+  test "changeset with blank email and username" do
+    %{errors: errors} = User.changeset(%User{}, %{})
+    assert {:email, {"can't be blank", [validation: :required]}} in errors
+    assert {:username, {"can't be blank", [validation: :required]}} in errors
+  end
+
+  test "changeset with invalid email format" do
+    %{errors: errors} = User.changeset(%User{}, %{email: "foo", username: "test"})
+    assert {:email, {"has invalid format", [validation: :format]}} in errors
+  end
+
+  test "changeset with username invalid length" do
+    long_username = String.duplicate "f", 21
+    %{errors: errors} = User.changeset(%User{}, %{username: long_username})
+    assert {:username, {"should be at most %{count} character(s)", [count: 20, validation: :length, max: 20]}} in errors
+  end
+
+  test "changeset must have a unique email" do
+    changeset = User.changeset(%User{}, @valid_attrs)
+    Repo.insert!(changeset)
+
+    changeset = User.changeset(%User{}, @valid_attrs)
+    {:error, changeset} = Repo.insert(changeset)
+    assert {:email, {"has already been taken", []}} in changeset.errors
+  end
+
+  test "changeset must have a unique username" do
+    changeset = User.changeset(%User{}, @valid_attrs)
+    Repo.insert!(changeset)
+
+    attrs = Map.put(@valid_attrs, :email, "test@bar.com")
+    changeset = User.changeset(%User{}, attrs)
+    {:error, changeset} = Repo.insert(changeset)
+    assert {:username, {"has already been taken", []}} in changeset.errors
+  end
+
+  test "registration changeset with valid attributes" do
+    valid_attrs = Map.put(@valid_attrs, :password, "password")
+    changeset = User.registration_changeset(%User{}, valid_attrs)
+    assert changeset.valid?
+    assert get_change(changeset, :encrypted_password)
+  end
+
+  test "registration changeset with invalid password length" do
+    long_password = String.duplicate "p", 101
+    %{errors: errors} = User.registration_changeset(%User{}, %{password: long_password})
+    assert {:password, {"should be at most %{count} character(s)", [count: 100, validation: :length, max: 100]}} in errors
+  end
 end
